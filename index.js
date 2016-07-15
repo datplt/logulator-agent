338cbb14d9e1f9b95f84e0f131d4073b243e3af9
pnhung177 <pnhung177@acegik.net>
2016-07-15 22:22:48 +0700

    Remove static HTML files; return service info

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var SERVER_PORT = 17779;
 
 var path = require('path');
+var fs = require('fs');
 var express = require('express');
 
 var debug = require('debug');
 var debuglog = debug('logulator:agent:main');
 
+var meta = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+
 var app = express();
 
-app.use('/', express.static(path.join(__dirname, './public')));
+app.use('/', function(req, res, next) {
+  debuglog(' + Request root-url, display metadata');
+  res.json({
+    name: meta.name,
+    version: meta.version,
+    author: meta.author
+  });
+});
 
 var server = app.listen(SERVER_PORT, '0.0.0.0', function () {
   var host = server.address().address;
