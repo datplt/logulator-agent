86006da9d0c14970d6f22f93c386de94a8883341
pnhung177 <pnhung177@acegik.net>
2016-07-19 21:29:45 +0700

    provide the service information (name, version, v.v.)

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var SERVER_PORT = 17779;
+'use strict';
+
+var SERVER_HOST = process.env.LOGULATOR_AGENT_HOST || '0.0.0.0';
+var SERVER_PORT = process.env.LOGULATOR_AGENT_PORT || 17779;
 
 var path = require('path');
 var express = require('express');
@@ -7,18 +10,19 @@ var debug = require('debug');
 var debuglog = debug('logulator:agent:main');
 
 var logUtil = require('./lib/log-util.js');
+var info = logUtil.getPackageInfo();
 
 var app = express();
 
 app.use('/', function(req, res, next) {
   debuglog(' + Request root-url, display metadata');
-  res.json(logUtil.getPackageInfo());
+  res.json(info);
 });
 
-var server = app.listen(SERVER_PORT, '0.0.0.0', function () {
+var server = app.listen(SERVER_PORT, SERVER_HOST, function () {
   var host = server.address().address;
   var port = server.address().port;
-  console.log('Logulator agent listening at http://%s:%s', host, port);
+  console.log('%s (%s) listening at http://%s:%s', info.name, info.version, host, port);
 });
 
 var ioserver = require('./lib/socketio-server.js');
